a3d9d30023e6ac367dc06de80ceb7bf9eeb6d8d8
dev-lan <devinscottlange@gmail.com>
2025-03-13 12:06:58 -0600

    constaint satisfication appears functional

--- template_expansion.py
+++ template_expansion.py
@@ -1,3 +1,4 @@
+from typing import List, Dict, Union
 import pandas as pd
 import re
 from constraint import *
@@ -7,95 +8,69 @@ from pprint import pprint
 
 def expand(df, dataset_schemas):
     expanded_rows = []
+    # TODO: optimize, flip the order of these for loops?
     for _, row in df.iterrows():
         for schema in dataset_schemas:
-            expanded_rows.extend(expand_template(row, schema))
+            schema_name = schema["name"]
+            schema_def = schema["schema"]
+            # flatten schema_def
+            schema_flattened = []
+            for file in schema_def:
+                entity = file["name"]
+                row_count = file["row_count"]
+                column_count = file["column_count"]
+                # TODO: FINISH FLATTENING
+                for col in file["columns"]:
+                    expanded_col = col.copy()
+                    expanded_col.update(
+                        {
+                            "entity": entity,
+                            "row_count": row_count,
+                            "column_count": column_count,
+                        }
+                    )
+                    schema_flattened.append(expanded_col)
+            field_options = schema_flattened
+            unique_entities = set([x["entity"] for x in schema_flattened])
+            empty_entity_options = [
+                {"name": None, "data_type": None, "cardinality": None}
+            ]
+            entity_options = [{"entity": entity} for entity in unique_entities]
+            for e in entity_options:
+                e.update(empty_entity_options)
+            expanded_rows.extend(expand_template(row, entity_options, field_options))
     expanded_df = pd.DataFrame(expanded_rows)
     return expanded_df
 
 
-def expand_template(row, schema):
-    # iterate over all possible field matches that satisfy the constraints.
-    # For each match, generate a new row with the template expanded.
+def expand_template(row, entity_options, field_options):
+    extract = extract_tags(row["query_template"])
+    tags = extract["tags"]
+    entities = extract["entities"]
+    fields = extract["fields"]
+    constraints = expand_constraints(row["constraints"], tags)
+    s = constraint_solver(entities, fields, constraints, entity_options, field_options)
+    return s
 
-    # For each possible constraint satifaction, expand the template
-    expanded_rows = []
-    return
 
-
-def get_satisfied_constraints(entities, fields, constraints, schema):
+def extract_tags(text: str) -> List[Dict[str, Union[str, List[str]]]]:
     """
-    Returns a list of possible satisfied constraints based on the provided entities, fields, constraints, and schema.
-    Args:
-        entities (list): A list of entities to be considered.
-        fields (list): A list of fields to be considered.
-        constraints (list): A list of constraints to be satisfied.
-        schema (dict): A dictionary representing the schema of the data.
-    Returns:
-        list: A list of dictionaries representing the satisfied constraints.
-              Each dictionary contains keys 'E' for entity and 'F' for field.
-    Example:
-        entities = ["E"]
-        fields = ["F1", "F2"]
-        constraints = [
-            {"field": "F1", "type": ["nominal", "ordinal"]},
-            {"field": "F2", "type": ["nominal", "ordinal"]},
-            {"F1.C > F2.C"}
+    Example input:
+     "This is just to test <E> and <E1> and <E2> and <F:O> and <E.F:O> and <E1.F1:N> and <E2.F2:O|N> and more text"
+    Example output:
+        [
+            {"original": "E", "entity": "E", "field": None, "field_type": None},
+            {"original": "E1", "entity": "E1", "field": None, "field_type": None},
+            {"original": "E2", "entity": "E2", "field": None, "field_type": None},
+            {"original": "F:O", "entity": None, "field": "F", "field_type": ["O"]},
+            {"original": "E1.F1:N", "entity": "E1", "field": "F1", "field_type": ["N"]},
+            {"original": "E2.F2:O|N", "entity": "E2", "field": "F2", "field_type": ["O", "N"]}
         ]
-        schema = {
-            "Country": ["Population", "GDP", "Area"],
-            "City": ["Population", "Area"]
-        }
-        result = get_satisfied_constraints(entities, fields, constraints, schema)
-        # result might be:
-        # [{"E": "Country", "F": "Population"}, {"E": "Country", "F": "GDP"}]
-    """
-
-    # returns a list of possible satisfied constraints
-    # e.g [{E: "Country", F: "Population"}, {E: Country, F: "GDP"}, ...]
-    return
-
-
-def is_constraint_satisfied(entities, fields, constraint, solution):
-    return
-
-
-def test_constraint_solver():
-    problem = Problem()
-    options = [
-        {"data_type": "nominal", "name": "A", "cardinality": 3},
-        {"data_type": "nominal", "name": "B", "cardinality": 18},
-        {"data_type": "quantitative", "name": "C", "cardinality": 5},
-        {"data_type": "quantitative", "name": "D", "cardinality": 23},
-    ]
-    problem.addVariables(["F1", "F2"], options)
-    # problem.addVariable("F2", options)
-    problem.addConstraint("F1['type'] == 'nominal'")
-    problem.addConstraint("F1['cardinality'] > F2['cardinality']")
-    problem.addConstraint("F2['cardinality'] > 4")
-    s = problem.getSolutions()
-    names = [[[k, v["name"]] for k, v in x.items()] for x in s]
-    # result: [[['F1', 'B'], ['F2', 'C']]]
-    print(names)
-    return
-
-
-def test_custom_parser():
-    text = "This is just to test <E> and <E1> and <E2> and <F:O> and <E.F:O> and <E1.F1:N> and <E2.F2:O|N> and more text"
-    """should extract
-    [
-        {"entity": "E", "field": None, "field_type": None},
-        {"entity": "E1", "field": None, "field_type": None},
-        {"entity": "E2", "field": None, "field_type": None},
-        {"entity": None, "field": "F", "field_type": "O"},
-        {"entity": "E1", "field": "F1", "field_type": "N"},
-        {"entity": "E2", "field": "F2", "field_type": "(O|N)"}
-    ]
     """
     pattern = r"<([^>]+)>"
     matches = re.findall(pattern, text)
 
-    result = []
+    tags = []
     for match in matches:
         parts = match.split(".")
         entity, field, field_type = None, None, None
@@ -116,13 +91,16 @@ def test_custom_parser():
             field_parts = field.split(":")
             if len(field_parts) == 2:
                 field, field_type = field_parts
-                field_type = field_type.split("|")
+                field_type = [
+                    {"N": "nominal", "O": "ordinal", "Q": "quantitative"}[t]
+                    for t in field_type.split("|")
+                ]
             else:
                 raise ValueError(
                     f"Invalid match: {match}. Field type must be specified"
                 )
 
-        result.append(
+        tags.append(
             {
                 "entity": entity,
                 "field": field,
@@ -130,26 +108,217 @@ def test_custom_parser():
                 "original": match,
             }
         )
-    pprint(result)
-    return
+    infer_entity(tags)
+    entities = set([tag["entity"] for tag in tags])
+    # fields = set([tag["field"] for tag in tags if tag["field"]])
+    fields = set(
+        [str(tag["entity"]) + "_" + tag["field"] for tag in tags if tag["field"]]
+    )
+    return {"tags": tags, "entities": list(entities), "fields": list(fields)}
 
 
-def test_grammar_parser():
-    grammar = Grammar("""
-    tag = tag_start entity.field:field_type tag_end
-    tag_start = '<'
-    tag_end = '>'
-    entity = E
-    field = ~r'[A-Za-z0-9]+'
-    field_type = ~r'[A-Za-z0-9]+'
-""")
-    print(grammar.parse("How many <E> are there, grouped by <E.F:N>?"))
+def infer_entity(
+    tags: List[Dict[str, Union[str, List[str]]]],
+) -> List[Dict[str, Union[str, List[str]]]]:
+    """
+    Infer the based on the other entities. If none is provided, default to E.
+    If there is an empty entity and multiple other entities defined, thwrow an error.
+    """
+    defined_entities = [tag["entity"] for tag in tags if tag["entity"]]
+    unique_entities = set(defined_entities)
+
+    fill_with = unique_entities.pop() if len(unique_entities) == 1 else "E"
+    if len(unique_entities) > 1 and any(not tag["entity"] for tag in tags):
+        raise ValueError("Multiple entities defined, cannot infer empty entity.")
+    for tag in [x for x in tags if not x["entity"]]:
+        tag["entity"] = fill_with
+    return tags
+
+
+def expand_constraints(
+    contstraints: List[str], tags: List[Dict[str, Union[str, List[str]]]]
+) -> List[str]:  # type: ignore
+    """
+    the current constraints will be expanded a bit
+        e.g. F.C > 4 → F["cardinality"] > 4
+    the tags will add constraints for each field type
+    and will add a constraint to ensure unique fields
+    """
+    expanded_constraints = []
+    # TODO expand input constraints
+
+    # Turn field types into constraints
+    expanded_constraints.extend(
+        [
+            f"{tag['entity']}_{tag['field']}['data_type'] in {tag['allowed_fields']}"
+            for tag in tags
+            if tag["field"]
+        ]
+    )
+
+    # Ensure fields are not repeated
+    unique_fields = set(
+        [str(tag["entity"]) + "_" + tag["field"] for tag in tags if tag["field"]]
+    )
+    # if len(unique_fields) > 1:
+    #     for field in unique_fields:
+    #         other_fields = unique_fields - {field}
+    #         expanded_constraints.append(f"{field} not in {other_fields}")
+
+    if len(unique_fields) > 1:
+        for field in unique_fields:
+            other_fields = unique_fields - {field}
+            name_str = "['name']"
+            other_fields_string = (
+                "[" + ",".join([str(x) + name_str for x in other_fields]) + "]"
+            )
+            expanded_constraints.append(
+                f"{field + name_str} not in {other_fields_string}"
+            )
+
+    # ensure that entities are not repeated
+    unique_entities = set([tag["entity"] for tag in tags])
+    if len(unique_entities) > 1:
+        for entity in unique_entities:
+            other_entities = unique_entities - {entity}
+            e_str = "['entity']"
+            other_entities_string = (
+                "[" + ",".join([str(x) + e_str for x in other_entities]) + "]"
+            )
+            expanded_constraints.append(
+                f"{entity + e_str} not in {other_entities_string}"
+            )
+
+    # ensure that fields belong to their entity
+    for field in unique_fields:
+        entity = field.split("_")[0]
+        expanded_constraints.append(f"{field}['entity'] == {entity}['entity']")
+
+    # TODO: do I need this one now?
+    # ensure that fields within the same entity are preserved
+    # unique_entities = set([tag["entity"] for tag in tags if tag["entity"]])
+    # # for entity in unique_entities:
+    #     entity_fields = [tag["field"] for tag in tags if tag["entity"] == entity]
+    #     if len(entity_fields) > 1:
+    #         expanded_constraints.append("==".join(f"{entity_fields}['entity']"))
+
+    return expanded_constraints
+
+
+def constraint_solver(
+    entities: List[str],
+    fields: List[str],
+    constraints: List[str],
+    entity_options: List[Dict[str, Union[str, int]]],
+    field_options: List[Dict[str, Union[str, int]]],
+) -> List[Dict[str, str]]:
+    problem = Problem()
+    print("⭐ constraints ⭐")
+    pprint(constraints)
+    print("⭐ entities ⭐")
+    pprint(entities)
+    pprint(entity_options)
+    print("⭐ fields ⭐")
+    pprint(fields)
+    pprint(field_options)
+    problem.addVariables(fields, field_options)
+    problem.addVariables(entities, entity_options)
+    for constraint in constraints:
+        problem.addConstraint(constraint)
+    s = problem.getSolutions()
+    pprint("⭐ solutions ⭐")
+    pprint(s)
+    return s
+
+
+def test_constraint_solver():
+    problem = Problem()
+    fields = [
+        {"entity": "donors", "data_type": "nominal", "name": "A", "cardinality": 3},
+        {"entity": "donors", "data_type": "nominal", "name": "B", "cardinality": 18},
+        {
+            "entity": "samples",
+            "data_type": "quantitative",
+            "name": "C",
+            "cardinality": 5,
+        },
+        {
+            "entity": "samples",
+            "data_type": "quantitative",
+            "name": "D",
+            "cardinality": 23,
+        },
+    ]
+    entities = [
+        {"entity": "donors", "data_type": None, "name": None, "cardinality": 200},
+        {"entity": "samples", "data_type": None, "name": None, "cardinality": 200},
+    ]
+    problem.addVariables(["F1"], fields)
+    problem.addVariables(["E1", "E2"], entities)
+    # problem.addConstraint("F1['data_type'] in {'nominal'}")
+    # problem.addConstraint("F1['entity'] == E1['entity']")
+    problem.addConstraint("E1['entity'] != E2['entity']")
+    # problem.addConstraint("F1['cardinality'] > F2['cardinality']")
+    # problem.addConstraint("F2['cardinality'] > 4")
+    # problem.addConstraint("F1['entity'] == F2['entity']== 'samples'")
+    s = problem.getSolutions()
+    names = [[{k: v["name"], "ent": v["entity"]} for k, v in x.items()] for x in s]
+    # result: [[['F1', 'B'], ['F2', 'C']]]
+    pprint(names)
     return
 
 
 if __name__ == "__main__":
-    test_custom_parser()
+    # test_custom_parser()
     # test_grammar_parser()
+    # test_constraint_solver()
+    # def expand_template(row, entity_options, field_options):
+    expand_template(
+        row={
+            "constraints": [],
+            "query_template": "waht <E1> hufh and <E2.F:Q>",
+        },
+        entity_options=[
+            {
+                "entity": "fake_sample",
+                "name": None,
+                "data_type": None,
+                "cardinality": None,
+            },
+            {
+                "entity": "fake_donor",
+                "name": None,
+                "data_type": None,
+                "cardinality": None,
+            },
+            {
+                "entity": "fake_file",
+                "name": None,
+                "data_type": None,
+                "cardinality": None,
+            },
+        ],
+        field_options=[
+            {
+                "entity": "fake_sample",
+                "name": "organ",
+                "data_type": "nominal",
+                "cardinality": 6,
+            },
+            {
+                "entity": "fake_donor",
+                "name": "weight",
+                "data_type": "quantitative",
+                "cardinality": 27,
+            },
+            {
+                "entity": "fake_donor",
+                "name": "height",
+                "data_type": "quantitative",
+                "cardinality": 27,
+            },
+        ],
+    )
 
     # # Example usage
     # data = {
